4d0d69ab7024f39a55a322f436faebe2ad2b0e9b
junewith2ns <165668059+junewith2ns@users.noreply.github.com>
2025-09-10 10:20:57 -0400

    Update Product.java

--- src/org/howard/edu/lsp/assignment2/Product.java
+++ src/org/howard/edu/lsp/assignment2/Product.java
@@ -1,1 +1,37 @@
+package org.howard.edu.lsp.assignment2;
+
+public class Product {
+    private int productId;
+    private String name;
+    private double price;
+    private String category;
+
+    public Product(int productId, String name, double price, String category) {
+        this.productId = productId;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    @Override
+    public String toString() {
+        return productId + "," + name + "," + price + "," + category;
+    }
+}
 
