b6897dbb45ff633f5ac7f3c22141e592b4606f69
junewith2ns <165668059+junewith2ns@users.noreply.github.com>
2025-09-10 10:14:04 -0400

    Create ETLPipeline.java
    Assignment2: add ETLPipeline skeleton

---
+++ src/org/howard/edu/lsp/assignment2/ETLPipeline.java
@@ -0,0 +1,7 @@
+package org.howard.edu.lsp.assignment2;
+
+public class ETLPipeline {
+    public static void main(String[] args) {
+        System.out.println("ETL Pipeline - Assignment 2");
+    }
+}
