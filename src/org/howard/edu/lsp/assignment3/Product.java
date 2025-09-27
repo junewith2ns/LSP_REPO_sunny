aa17f8aaed1802703abfa88132484851fa9450d0
junewith2ns <165668059+junewith2ns@users.noreply.github.com>
2025-09-26 22:50:25 -0400

    Update Product.java

--- src/org/howard/edu/lsp/assignment3/Product.java
+++ src/org/howard/edu/lsp/assignment3/Product.java
@@ -1,14 +1,25 @@
 package org.howard.edu.lsp.assignment3;
 
-// One raw input row from products.csv.
-// Immutable after construction.
-
+/**
+ * Product represents one raw input row from products.csv.
+ * <p>
+ * Immutable after construction.
+ * </p>
+ */
 public class Product {
     private final int productId;
     private final String name;
     private final double price;
     private final String category;
 
+    /**
+     * Build a Product object.
+     * 
+     * @param productId unique product ID
+     * @param name product name
+     * @param price product price
+     * @param category product category
+     */
     public Product(int productId, String name, double price, String category) {
         this.productId = productId;
         this.name = name;
@@ -16,11 +27,15 @@ public class Product {
         this.category = category;
     }
 
+    /** @return the product ID */
     public int getProductId() { return productId; }
 
+    /** @return the product name */
     public String getName() { return name; }
     
+    /** @return the product price */
     public double getPrice() { return price; }
     
+    /** @return the product category */
     public String getCategory() { return category; }
 }
