030d34c95cf40cac52a6e5ac560eb54b10a380e2
junewith2ns <165668059+junewith2ns@users.noreply.github.com>
2025-12-08 19:46:51 -0500

    debug

--- src/org/howard/edu/lsp/final/question1/EnhancedAlgorithm.java
+++ src/org/howard/edu/lsp/final/question1/EnhancedAlgorithm.java
@@ -2,18 +2,17 @@ package org.howard.edu.lsp.finale.question1;
 
 import java.security.SecureRandom;
 
-/**
- * Alphanumeric generator (A–Z, a–z, 0–9) using SecureRandom.
- */
+/** A–Z, a–z, 0–9 using SecureRandom. */
 class EnhancedAlgorithm implements PasswordAlgorithm {
     public String generate(int length) {
-        String allowed = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
-                         "abcdefghijklmnopqrstuvwxyz" +
-                         "0123456789";
+        String allowed = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                       + "abcdefghijklmnopqrstuvwxyz"
+                       + "0123456789";
         SecureRandom r = new SecureRandom();
         String result = "";
         for (int i = 0; i < length; i++) {
-            result += allowed.charAt(r.nextInt(allowed.length()));
+            int index = r.nextInt(allowed.length());
+            result += allowed.charAt(index);   // naive concat
         }
         return result;
     }
