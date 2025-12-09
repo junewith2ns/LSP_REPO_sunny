a3e55e4097ce78dcbda99b298e3c8e6539c7c41e
junewith2ns <165668059+junewith2ns@users.noreply.github.com>
2025-12-08 20:14:20 -0500

    copy paste from previous directory named final

---
+++ src/org/howard/edu/lsp/finale/question1/BasicAlgorithm.java
@@ -0,0 +1,17 @@
+package org.howard.edu.lsp.finale.question1;
+
+import java.util.Random;
+
+/** Digits-only generator using java.util.Random. */
+class BasicAlgorithm implements PasswordAlgorithm {
+    public String generate(int length) {
+        String digits = "0123456789";
+        Random r = new Random();
+        String result = "";
+        for (int i = 0; i < length; i++) {
+            int index = r.nextInt(digits.length());
+            result += digits.charAt(index);   
+        }
+        return result;
+    }
+}
